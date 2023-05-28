894d8a9d7bb939c2494734d27487a2b3b60da0ee
artem <artem28282828@gmail.com>
2023-05-28 15:10:28 +0500

    Решение второй задачи

---
+++ Seminar002_HomeWork2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число: ");
+
+
+var stringNumber = Console.ReadLine();
+int number;
+
+bool isNumber = int.TryParse(stringNumber, out number);
+
+if (isNumber && stringNumber.Length >= 3)
+{
+Console.WriteLine("Третья цифра числа: " + stringNumber[2]);
+}
+else
+{
+Console.WriteLine("Тут нет третьего числа");
+
+}
